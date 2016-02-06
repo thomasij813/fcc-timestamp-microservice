01e284c8f4a9f0109e472cbbc34eb37ed893542b
Thomas Johnson <thomasij813@gmail.com>
2016-02-05 23:11:02 -0800

    configure jade and views

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+
+var routes = require('./routes/index.js');
 
 var port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
 
 app.listen(port, function() {
   console.log('Listening on port ' + port);
