64bbc5c8731bcb3c0bc03fa86ebc21bd47cef67b
Thomas Johnson <thomasij813@gmail.com>
2016-02-06 10:17:09 -0800

    Finish date conversion logic in get /:timestamp route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
+var moment = require('moment');
 var express = require('express');
+var chrono = require('chrono-node');
 var router = express.Router();
 
+
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('index');
+});
+
+router.get('/:timestamp', function(req, res, next) {
+  var timestamp = req.params.timestamp;
+  var dateObj = {};
+  // if timestamp is a unix date
+  if (/^\d*$/.exec(timestamp)) {
+    dateObj.natural = moment(parseInt(timestamp) * 1000).format('MMMM DD, YYYY');
+    dateObj.unix = timestamp;
+  }
+  // if timestamp is a natural language string
+  else {
+    var iso = chrono.parseDate(timestamp);
+    dateObj.natural = timestamp;
+    dateObj.unix = moment(iso).unix();
+  }
+  res.json(dateObj);
 });
 
 module.exports = router;
